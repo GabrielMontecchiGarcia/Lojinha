7fe6a98db60ce8e0b4cf720212f07cb35f9b3f5c
2024.4.14 <2024.4.14@DESKTOP-OMTIDSQ>
2026-05-15 15:46:11 -0400

    ProdutoBLL criado

--- Lojinha/BLL/ProdutosBLL.cs
+++ Lojinha/BLL/ProdutosBLL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +18,39 @@ namespace Lojinha.BLL
             {
                 throw new Exception("O Nome do Produto é Obrigátorio");
             }
-            produtos.Preco = produtos.Preco;
+            if(produtos.Preco == 0)
+            {
+                throw new Exception("O preço do produto é obrigátorio");
+            }
+            if(produtos.Estoque == 0)
+            {
+                throw new Exception("O estoque do produto é obrigátorio");
+            }
+            ProdutosBLL obj = new ProdutosBLL();
+            obj.Incluir(produtos);
+        }
+        public void Alterar (ProdutosInformation produtos)
+        {
+            if(produtos.Nome.Trim().Length == 0)
+            {
+                throw new Exception("O nome do produto é obrigátorio");
+            }
+            ProdutosBLL obj = new ProdutosBLL();
+            obj.Alterar(produtos);
+        }
+        public void Excluir(int codigo)
+        {
+            if(codigo < 1)
+            {
+                throw new Exception("Selecione um produto válido antes de excluir");
+            }
+            ProdutosBLL obj = new ProdutosBLL();
+            obj.Excluir(codigo);
+        }
+        public DataTable Listagem(string filtro)
+        {
+            ProdutosBLL obj = new ProdutosBLL();
+            return obj.Listagem(filtro);
         }
     }
 }
